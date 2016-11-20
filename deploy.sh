4d077c9be056d1c128b90d4e86308a5b4941443d
EC2 Default User <alex.cook@weavescape.com.au>
2016-11-20 00:08:25 +0000

    Backup worlds model on deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 pkill node
+
+mv ~/web-server/models/worlds.json ~/web-server/models/worlds.json.bak
+
 git pull
 
 sudo cp -R src/client/webapp/* /var/www/html
@@ -9,5 +12,8 @@ sudo cp -R src/server/* ~/web-server/
 sudo chown -R ec2-user ~/web-server/
 
 cd ~/web-server/
+
+mv models/worlds.json models/worlds.json.example
+mv models/worlds.json.bak models/worlds.json
 
 node Server.js &
